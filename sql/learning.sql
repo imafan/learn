a9e984b9e5e1c1bcbfb034c0b119c0a270648184
imafan <358462953@qq.com>
2015-11-27 17:36:14 +0800

    test

--- sql/learning.sql
+++ sql/learning.sql
@@ -18,14 +18,15 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for `blog`
 -- ----------------------------
-DROP TABLE IF EXISTS `blog`;
-CREATE TABLE `blog` (
+DROP TABLE IF EXISTS `t_blog`;
+CREATE TABLE `t_blog` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(125) NOT NULL,
   `content` blob,
   `description` varchar(256) DEFAULT NULL,
   `userId` int(11) DEFAULT NULL,
   `createTime` datetime DEFAULT NULL,
+  `status` int(2) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -36,8 +37,8 @@ CREATE TABLE `blog` (
 -- ----------------------------
 -- Table structure for `user`
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
